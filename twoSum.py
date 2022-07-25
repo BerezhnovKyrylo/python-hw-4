f528fe6207cd794d203f468e299429ebb384e7a0
Berezhnov Kyrylo <berethnov@gmail.com>
2022-07-25 13:05:18 +0300

    HW4

--- twoSum.py
+++ twoSum.py
@@ -15,3 +15,19 @@
 #twoSum([2, 7, 11, 15], 9)   # [0, 1]
 #twoSum([3, 2, 4], 6)        # [1, 2]
 #twoSum([3, 3], 6)           # [0, 1]
+
+from typing import List
+
+
+def two_sum(nums: List[int], target: int):
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                print([i, j])
+                return
+
+
+two_sum([1, 2, 3], 4)
+two_sum([2, 7, 11, 15], 9)
+two_sum([3, 2, 4], 6)
+two_sum([3, 3], 6)
